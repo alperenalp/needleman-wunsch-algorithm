4ddbd0ae82ee74a6ebd62b785a4c422ccd777be5
Alperen <Alperen@DESKTOP-DV6P2KS>
2023-03-13 18:10:21 +0300

    algorithm completed

--- NeedlemanWunschAlgorithm/Form1.Designer.cs
+++ NeedlemanWunschAlgorithm/Form1.Designer.cs
@@ -29,26 +29,180 @@
         private void InitializeComponent()
         {
             tabControl1 = new TabControl();
+            tabPage2 = new TabPage();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label6 = new Label();
+            label5 = new Label();
             tabPage1 = new TabPage();
             dataGridView1 = new DataGridView();
-            tabPage2 = new TabPage();
-            label1 = new Label();
             tabControl1.SuspendLayout();
+            tabPage2.SuspendLayout();
             tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            tabPage2.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
             // 
-            tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage1);
             tabControl1.Dock = DockStyle.Fill;
             tabControl1.Location = new Point(0, 0);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
             tabControl1.Size = new Size(684, 661);
             tabControl1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(button1);
+            tabPage2.Controls.Add(textBox3);
+            tabPage2.Controls.Add(label4);
+            tabPage2.Controls.Add(textBox2);
+            tabPage2.Controls.Add(label3);
+            tabPage2.Controls.Add(textBox1);
+            tabPage2.Controls.Add(label2);
+            tabPage2.Controls.Add(label1);
+            tabPage2.Controls.Add(label7);
+            tabPage2.Controls.Add(label8);
+            tabPage2.Controls.Add(label6);
+            tabPage2.Controls.Add(label5);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(676, 633);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Anasayfa";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(220, 241);
+            button1.Name = "button1";
+            button1.Size = new Size(171, 52);
+            button1.TabIndex = 11;
+            button1.Text = "HESAPLA";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(220, 170);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(64, 43);
+            textBox3.TabIndex = 10;
+            textBox3.Text = "-2";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(130, 167);
+            label4.Name = "label4";
+            label4.Size = new Size(84, 45);
+            label4.TabIndex = 9;
+            label4.Text = "Gap:";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(220, 125);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(64, 43);
+            textBox2.TabIndex = 8;
+            textBox2.Text = "-1";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(48, 123);
+            label3.Name = "label3";
+            label3.Size = new Size(166, 45);
+            label3.TabIndex = 7;
+            label3.Text = "Mismatch:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(220, 80);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(64, 43);
+            textBox1.TabIndex = 6;
+            textBox1.Text = "1";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(98, 77);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 45);
+            label2.TabIndex = 5;
+            label2.Text = "Match:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(59, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(559, 45);
+            label1.TabIndex = 4;
+            label1.Text = "NEEDLEMAN WUNSCH ALGORITMASI";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(91, 432);
+            label7.Name = "label7";
+            label7.Size = new Size(124, 45);
+            label7.TabIndex = 3;
+            label7.Text = "Score:  ";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(91, 518);
+            label8.Name = "label8";
+            label8.Size = new Size(117, 25);
+            label8.TabIndex = 2;
+            label8.Text = "Geçen Süre: ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(91, 387);
+            label6.Name = "label6";
+            label6.Size = new Size(115, 45);
+            label6.TabIndex = 1;
+            label6.Text = "Seq2:  ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(91, 342);
+            label5.Name = "label5";
+            label5.Size = new Size(115, 45);
+            label5.TabIndex = 0;
+            label5.Text = "Seq1:  ";
             // 
             // tabPage1
             // 
@@ -58,7 +212,7 @@
             tabPage1.Padding = new Padding(3);
             tabPage1.Size = new Size(676, 633);
             tabPage1.TabIndex = 0;
-            tabPage1.Text = "tabPage1";
+            tabPage1.Text = "Tablo";
             tabPage1.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
@@ -81,26 +235,6 @@
             dataGridView1.Size = new Size(670, 627);
             dataGridView1.TabIndex = 3;
             // 
-            // tabPage2
-            // 
-            tabPage2.Controls.Add(label1);
-            tabPage2.Location = new Point(4, 24);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(676, 633);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "tabPage2";
-            tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(158, 71);
-            label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
-            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -110,12 +244,11 @@
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
-            Load += Form1_Load;
             tabControl1.ResumeLayout(false);
-            tabPage1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             tabPage2.ResumeLayout(false);
             tabPage2.PerformLayout();
+            tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
@@ -125,6 +258,17 @@
         private TabPage tabPage1;
         private DataGridView dataGridView1;
         private TabPage tabPage2;
+        private Label label5;
+        private Label label6;
+        private Label label8;
+        private Label label7;
+        private Button button1;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label2;
         private Label label1;
     }
 }
--- NeedlemanWunschAlgorithm/Form1.cs
+++ NeedlemanWunschAlgorithm/Form1.cs
@@ -11,12 +11,32 @@ namespace NeedlemanWunschAlgorithm
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private int CalculateMatchState(int i, int j)
         {
+            int matchState = 0;
+            int match = Convert.ToInt32(textBox1.Text);
+            int mismatch = Convert.ToInt32(textBox2.Text);
+            if (dataGridView1.Rows[0].Cells[i].Value.Equals(dataGridView1.Rows[j].Cells[0].Value))
+            {
+                matchState = match;
+            }
+            else
+            {
+                matchState = mismatch;
+            }
+            return matchState;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label5.Text = "Seq1:  ";
+            label6.Text = "Seq2:  ";
+            label7.Text = "Score:  ";
             Stopwatch timer = new Stopwatch();
             timer.Start();
             try
             {
+                // dosya islemleri
                 int seq1Length = 0;
                 string seq1Text = string.Empty;
                 int seq2Length = 0;
@@ -42,12 +62,13 @@ namespace NeedlemanWunschAlgorithm
 
                 }
 
+                // tablo boyutu
                 int tableColumnLength = seq1Length + 2;
                 int tableRowLength = seq2Length + 2;
                 dataGridView1.ColumnCount = tableColumnLength;
                 dataGridView1.RowCount = tableRowLength;
 
-                // satýrlarýn yüksekliðini tabloya sýðdýrma
+                // satirlarin yuksekligini tabloya sigdirma
                 foreach (DataGridViewRow row in dataGridView1.Rows)
                 {
                     row.Height = Convert.ToInt32((dataGridView1.ClientRectangle.Height - dataGridView1.ColumnHeadersHeight) / (double)(dataGridView1.Rows.Count - 0.3));
@@ -65,8 +86,8 @@ namespace NeedlemanWunschAlgorithm
                 }
 
 
-                // hesaplama iþlemleri yapýlmasý
-                //
+                // hesaplama islemlerinin yapilmasi
+
                 // match = +1
                 // mismatch = -1;
                 // gap = -2
@@ -75,44 +96,43 @@ namespace NeedlemanWunschAlgorithm
                 //                 (T[i-1, j] + gap) ||
                 //                 (T[i, j-1] + gap)
 
-
                 dataGridView1.Rows[1].Cells[1].Value = 0;
-                // ilk satýrýn hücrelerinin doldurulmasý
-                int gap = -2;
+                // ilk satirin hucrelerinin doldurulmasi
+                int gapPenalty = Convert.ToInt32(textBox3.Text); ;
                 for (int i = 2; i < tableColumnLength; i++)
                 {
                     // ikinci formul
                     int cellValue = Convert.ToInt32(dataGridView1.Rows[1].Cells[i - 1].Value);
-                    dataGridView1.Rows[1].Cells[i].Value = cellValue + gap;
+                    dataGridView1.Rows[1].Cells[i].Value = cellValue + gapPenalty;
                 }
-                // ilk kolonun hücrelerinin doldurulmasý
+                // ilk kolonun hucrelerinin doldurulmasi
                 for (int j = 2; j < tableRowLength; j++)
                 {
-                    // üçüncü formul
+                    // ucuncu formul
                     int cellValue = Convert.ToInt32(dataGridView1.Rows[j - 1].Cells[1].Value);
-                    dataGridView1.Rows[j].Cells[1].Value = cellValue + gap;
+                    dataGridView1.Rows[j].Cells[1].Value = cellValue + gapPenalty;
                 }
-                // diðer hücrelerin doldurulmasý
+                // diger hucrelerin doldurulmasi
                 for (int j = 1; j < tableRowLength; j++)
                 {
                     for (int i = 1; i < tableColumnLength; i++)
                     {
-                        if (i != 1 && j != 1) // ilk satýr ve sütun zaten dolduruldu
+                        if (i != 1 && j != 1) // ilk satýr ve sutun zaten dolduruldu
                         {
                             int[] results = new int[3];
                             // ilk formul
-                            // i == 2 ve j == 2 olduðunda bende tablo bazlarý içerdiði için T[1,1] ama bazlarý içermeyen tabloya göre T[0,0]. almak istediðim bu deðer ise 0 'dýr.
+                            // i == 2 ve j == 2 olduðunda bende tablo bazlari icerdigi icin T[1,1] ama bazlarý içermeyen tabloya göre T[0,0]. almak istedigim bu deðer ise 0 'dýr.
                             int cellValueOfCross = Convert.ToInt32(dataGridView1.Rows[j - 1].Cells[i - 1].Value);
                             int matchState = CalculateMatchState(i, j);
                             results[0] = cellValueOfCross + matchState;
                             // ikinci formul
                             int cellValueOfLeft = Convert.ToInt32(dataGridView1.Rows[j].Cells[i - 1].Value);
-                            results[1] = cellValueOfLeft + gap;
-                            // üçüncü formul
+                            results[1] = cellValueOfLeft + gapPenalty;
+                            // ucuncu formul
                             int cellValueOfTop = Convert.ToInt32(dataGridView1.Rows[j - 1].Cells[i].Value);
-                            results[2] = cellValueOfTop + gap;
+                            results[2] = cellValueOfTop + gapPenalty;
 
-                            // max deðeri bulma
+                            // max degeri bulma
                             int max = results[0];
                             for (int k = 0; k < results.Length; k++)
                             {
@@ -122,37 +142,158 @@ namespace NeedlemanWunschAlgorithm
                                 }
                             }
 
-                            // sonucu tabloya iþleme
+                            // sonucu tabloya isleme
                             dataGridView1.Rows[j].Cells[i].Value = max;
                         }
                     }
                 }
 
-                //MessageBox.Show("Geçen Zaman: " + timer.Elapsed.ToString() + "\nYani: " + timer.ElapsedMilliseconds.ToString() + " milisaniye");
-                timer.Stop();
+                // izlenecek yolun belirlenip indexlerin tutulmasi
+                int columnIndex = tableColumnLength - 1;
+                int rowIndex = tableRowLength - 1;
+                List<string> indexesOfMatchPath = new List<string>();
+                string pathIndex = columnIndex + rowIndex.ToString();
+                indexesOfMatchPath.Add(pathIndex);
+                while (true)
+                {
+                    // eslesiyorsa capraza git eslesmiyorsa maximumu bul
+                    if ((columnIndex != 1 && rowIndex != 1) && CalculateMatchState(columnIndex, rowIndex) == 1)
+                    {
+                        columnIndex = columnIndex - 1;
+                        rowIndex = rowIndex - 1;
+                        pathIndex = columnIndex + rowIndex.ToString();
+                        indexesOfMatchPath.Add(pathIndex);
+                    }
+                    else
+                    {
+                        // int bir deger mi karsilastir eðer int ise degeri guncelle deðilse minimum deðeri alsýn. Cunku en sol ve en ust taraflardan null deðer gelecek.
+                        int cellTopValue = -999;
+                        if (rowIndex - 1 != 0)
+                        {
+                            cellTopValue = Convert.ToInt32(dataGridView1.Rows[rowIndex - 1].Cells[columnIndex].Value);
+                        }
+                        int cellLeftValue = -999;
+                        if (columnIndex - 1 != 0)
+                        {
+                            cellLeftValue = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells[columnIndex - 1].Value);
+                        }
+                        int cellCrossValue = -999;
+                        if (rowIndex - 1 != 0 && columnIndex - 1 != 0)
+                        {
+                            cellCrossValue = Convert.ToInt32(dataGridView1.Rows[rowIndex - 1].Cells[columnIndex - 1].Value);
+                        }
+
+                        // bulunan degeleri karsilastir maximumu bul
+                        if (cellCrossValue >= cellLeftValue && cellCrossValue >= cellTopValue) // maximum deger caprazda
+                        {
+                            columnIndex = columnIndex - 1;
+                            rowIndex = rowIndex - 1;
+                            pathIndex = columnIndex + rowIndex.ToString();
+                            indexesOfMatchPath.Add(pathIndex);
+                        }
+                        else if (cellLeftValue >= cellCrossValue && cellLeftValue >= cellTopValue) // maximum solda
+                        {
+                            columnIndex = columnIndex - 1;
+                            rowIndex = rowIndex;
+                            pathIndex = columnIndex + rowIndex.ToString();
+                            indexesOfMatchPath.Add(pathIndex);
+                        }
+                        else  // maximum yukarda
+                        {
+                            columnIndex = columnIndex;
+                            rowIndex = rowIndex - 1;
+                            pathIndex = columnIndex + rowIndex.ToString();
+                            indexesOfMatchPath.Add(pathIndex);
+                        }
+                    }
+                    if (columnIndex == 1 && rowIndex == 1)
+                    {
+                        break;
+                    }
+                }
+
+                // yollarýn indexleri alinip yeni eslestirmenin yapilmasi 
+                indexesOfMatchPath.Reverse();
+                string newSeq1 = string.Empty;
+                string newSeq2 = string.Empty;
+                int tempIndexOfSeq1 = 0;
+                int tempIndexOfSeq2 = 0;
+                for (int i = 0; i < indexesOfMatchPath.Count(); i++)
+                {
+                    if (indexesOfMatchPath[i] != "11")
+                    {
+                        // baz indexini int'e donustur
+                        string index = indexesOfMatchPath[i];
+                        int seq1BaseIndex = Convert.ToInt32(index[0].ToString());
+                        int seq2BaseIndex = Convert.ToInt32(index[1].ToString());
+
+                        string baseOfSeq1 = string.Empty;
+                        string baseOfSeq2 = string.Empty;
+                        if (dataGridView1.Rows[0].Cells[seq1BaseIndex].Value != null)
+                        {
+                            baseOfSeq1 = dataGridView1.Rows[0].Cells[seq1BaseIndex].Value.ToString();
+                        }
+                        if (dataGridView1.Rows[seq2BaseIndex].Cells[0].Value != null)
+                        {
+                            baseOfSeq2 = dataGridView1.Rows[seq2BaseIndex].Cells[0].Value.ToString();
+                        }
+
+                        // eger tekrar eden index varsa tekrar kullanma ve boþ ise - koy deðilse degeri ata
+                        if (tempIndexOfSeq1 == seq1BaseIndex || baseOfSeq1 == "")
+                        {
+                            newSeq1 += "-";
+                        }
+                        else
+                        {
+                            newSeq1 += baseOfSeq1;
+                        }
+
+                        if (tempIndexOfSeq2 == seq2BaseIndex || baseOfSeq2 == "")
+                        {
+                            newSeq2 += "-";
+                        }
+                        else
+                        {
+                            newSeq2 += baseOfSeq2;
+                        }
+
+                        tempIndexOfSeq1 = seq1BaseIndex;
+                        tempIndexOfSeq2 = seq2BaseIndex;
+                    }
+                }
+                label5.Text += newSeq1;
+                label6.Text += newSeq2;
+
+                // score hesaplanmasý
+                int match = Convert.ToInt32(textBox1.Text);
+                int mismatch = Convert.ToInt32(textBox2.Text);
+                int gapPoint = Convert.ToInt32(textBox3.Text);
+                int score = 0;
+                for (int i = 0; i < newSeq1.Length; i++)
+                {
+                    if (newSeq1[i].Equals(newSeq2[i]))
+                    {
+                        score += match;
+                    }
+                    else if (newSeq1[i] == '-' || newSeq2[i] == '-')
+                    {
+                        score += gapPoint;
+                    }
+                    else
+                    {
+                        score += mismatch;
+                    }
+                }
+                label7.Text += score.ToString();
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Bir hatayla karþýlaþýldý...\n{ex}");
             }
 
+            timer.Stop();
+            label8.Text = "Geçen Süre: " + timer.Elapsed.ToString() + "  ------>  " + timer.ElapsedMilliseconds.ToString() + " milisaniye";
         }
-
-        private int CalculateMatchState(int i, int j)
-        {
-            int matchState = 0;
-            int match = 1;
-            int mismatch = -1;
-            if (dataGridView1.Rows[0].Cells[i].Value.Equals(dataGridView1.Rows[j].Cells[0].Value))
-            {
-                matchState = match;
-            }
-            else
-            {
-                matchState = mismatch;
-            }
-            return matchState;
-        }
-
     }
 }
